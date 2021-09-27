d6ef2fead996c21a228b5d1c7dac8317f89fb085
amemem <ameme698@student.liu.se>
2021-09-27 19:58:07 +0200

    Add documentation

---
+++ R/lab5package-package.R
@@ -0,0 +1,7 @@
+#' @title 2014 Swedish elections: county results
+#'
+#' @description This package contains a reference class that calls the Valmyndigheten API, extracts all the county votes and calculates the percentage of votes for each political party.
+#' See: \code{\link{val2014}}
+#'
+#' @name lab5package
+"_PACKAGE"
--- R/val2014-class.R
+++ R/val2014-class.R
@@ -1,3 +1,36 @@
+#' @title The \code{val2014} Reference Class
+#'
+#' @description A Reference Class to calculate the 2014 Swedish election results for all counties. It calls the Valmyndigheten API, reads the semi-colon separated values file, extracts all the votes for each party in every municipality, and calculates the percentage of votes for each political party in every county. These results are stored in the respective county name field.
+#'
+#' @field Blekinge Blekinge County
+#' @field Dalarna Dalarna County
+#' @field Gotland Gotland county
+#' @field Gavleborg Gävleborg county
+#' @field Halland Halland county
+#' @field Jamtland Jämtland county
+#' @field Jonkoping Jönköping county
+#' @field Kalmar Kalmar county
+#' @field Kronoberg Kronoberg county
+#' @field Norrbotten Norrbotten county
+#' @field Skane Skåne county
+#' @field Stockholm Stockholm county
+#' @field Sodermanland Södermanland county
+#' @field Uppsala Uppsala county
+#' @field Varmland Värmland county
+#' @field Vasterbotten Västerbotten county
+#' @field Vasternorrland Västernorrland county
+#' @field Vastmanland Västmanland county
+#' @field vastergotland Västra Götaland county
+#' @field Orebro Örebro county
+#' @field Ostergotland Östergötland county
+#'
+#' @return A \code{val2014} class generator object.
+#' @export val2014
+#' @name val2014
+#'
+#' @examples
+#' val = val2014()
+#' print(val$county("Blekinge"))
 val2014 = setRefClass(
   "val2014",
   fields = list(
@@ -25,6 +58,7 @@ val2014 = setRefClass(
   ),
   methods = list(
     initialize = function() {
+      "Calls the Valmyndigheten API to get the 2014 election results of all municipality, then calculating all the election results for every county and storing them in this object."
       val = read.csv2("https://data.val.se/val/val2014/statistik/2014_riksdagsval_per_kommun.skv")
       stopifnot(
         length(val) == 32,
@@ -111,6 +145,7 @@ val2014 = setRefClass(
       }
     },
     county = function(county) {
+      "Returns a named vector with percent values of all political parties for a given county."
       return(.self$field(county))
     }
   )
